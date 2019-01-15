3c3758d4a3d854ab775285a32fb1a89df583dae9
nikita-skobov <nskobov.websites@gmail.com>
2019-01-15 14:24:58 -0600

    sets up react, and react dependencies

--- src/entry.js
+++ src/entry.js
@@ -1,1 +1,8 @@
-console.log('aayyyyyyyyyyyyyyyyyy')
+/* global document */
+import '@babel/polyfill'
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const reactContainer = document.getElementById('react-container')
+
+ReactDOM.render(<div>hey lme4512121212121244aoo</div>, reactContainer)
