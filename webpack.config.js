8d00d8eb60f726ed6cdc27dcf7758b27df3036c0
nikita-skobov <nskobov.websites@gmail.com>
2019-01-15 14:04:10 -0600

    fixes public path for output scripts

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
   // },
   output: {
     path: path.resolve(__dirname, 'dist'),
-    publicPath: '/',
+    publicPath: './',
     filename: 'scripts/bundle.js',
     // chunkFileName: 'bundle.[contenthash].js',
   },
